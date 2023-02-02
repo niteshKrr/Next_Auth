82fd92fa2f00307f6f66ba543a9ca531784c5527
niteshKrr <nkumar35101@gmail.com>
2023-02-03 02:06:40 +0530

    minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 import { useSession, signIn, signOut } from "next-auth/react"
 
 
 export default function Home() {
-  const { data: session, status } = useSession()
-  const userEmail = session?.user?.email;
-
-  if (status === "loading") {
-    return <p>Hang on there...</p>
-  }
-
-  if (status === "authenticated") {
+  const { data: session } = useSession()
+  if (session) {
     return (
       <>
-        <p>Signed in as {userEmail}</p>
+        Signed in as {session?.user?.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
-        <img src="https://cdn.pixabay.com/photo/2017/08/11/19/36/vw-2632486_1280.png" />
       </>
     )
   }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
 
   return (
     <>
